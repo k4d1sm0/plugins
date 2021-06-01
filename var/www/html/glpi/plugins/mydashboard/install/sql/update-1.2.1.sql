78b5285e7dc4738b1f85e35e023f2d51c657c850
k4d1sm0 <victor.marques@truckpad.com.br>
2021-06-01 16:17:58 -0300

    removido ovar/

--- var/www/html/glpi/plugins/mydashboard/install/sql/update-1.2.1.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `glpi_plugin_mydashboard_alerts`
-  ADD `is_public` TINYINT(1) NOT NULL
-  AFTER `impact`;
--- var/www/html/glpi/plugins/mydashboard/install/sql/update-1.3.3.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `glpi_plugin_mydashboard_alerts`
-  ADD `type` TINYINT(1) NOT NULL
-  AFTER `impact`;
